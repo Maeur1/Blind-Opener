c26c5b685b73f1472bc4ddff335185afab498e44
Mayur Panchal <panchal.mayur2@gmail.com>
2020-07-04 13:33:30 +1000

    Got it working now

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,17 @@
 // ESP8266 Usable Pins: 16, 15, 14, 13, 12, 5, 4, 3, 2, 1, 0
 
 #define EN_PIN 16
-#define DIR_PIN 4  //			19			55	//direction
-#define STEP_PIN 5 //			18			54	//step
+#define DIR_PIN 15
+#define STEP_PIN 1
 #define MOSI_PIN 13
 #define MISO_PIN 12
 #define CS_PIN 15
 #define SCK_PIN 14
 #define MOTOR_STEPS 200 // Number of steps per rotation
-#define MICROSTEPS 256
-#define MAX_DELTA MICROSTEPS
-#define MIN_DELTA -MICROSTEPS
+#define MICROSTEPS 128
+#define MAX_DELTA MICROSTEPS / 4
+#define MIN_DELTA -MICROSTEPS / 4
+#define DEADZONE 600
 #define ESP_LED 2
 
 #include "BasicStepperDriver.h"
@@ -27,12 +28,14 @@
 #include <ArduinoJson.h> // required for settings file to make it readable
 #include <FS.h>
 #include <ESPmanager.h>
+#include <AS5600.h>
 
 ESP8266WebServer HTTP(80);
 BasicStepperDriver stepper = BasicStepperDriver(MOTOR_STEPS, DIR_PIN, STEP_PIN);
-TMC2130Stepper driver = TMC2130Stepper(EN_PIN, DIR_PIN, STEP_PIN, CS_PIN);
+TMC2130Stepper driver = TMC2130Stepper(EN_PIN, DIR_PIN, STEP_PIN, CS_PIN, MOSI_PIN, MISO_PIN, SCK_PIN);
 WiFiClient espClient;
 PubSubClient client(espClient);
+AS5600 encoder;
 
 const char *defaultSSID = "DefaultSSID";
 const char *defaultPSK = "DefaultPassword";
@@ -42,13 +45,16 @@ const char *topic_set = "esp8266/blinds/set";
 const char *topic_position = "esp8266/blinds/position";
 const char *topic_feedback = "esp8266/blinds/feedback";
 const char *topic_set_position = "esp8266/blinds/set_position";
-const long blinds_top = 4150000;
-const long max_power = blinds_top * 4 / 6;
-const int blinds_bot = 0;
+const long encoder_top = 700;
+const long encoder_bottom = -179000;
+const long max_power = -80000;
+long revolutions = 0;
+double output;
+long lastOutput;
+long lastOvercurrent = 0;
 
-long taken = blinds_top; //Steps taken so far by the stepper motor
-long target = 0;		 //Steps wanted to be taken by the stepper motor
-long position = blinds_top;
+long target = 0; //Steps wanted to be taken by the stepper motor
+long position = 0;
 bool publish_position = false;
 
 ESPmanager settings(HTTP, SPIFFS, "ESPManager", defaultSSID, defaultPSK);
@@ -58,36 +64,36 @@ void motorStatus()
 	String stall = driver.stallguard() ? "STALLED" : "NOT STALLED";
 	String status = String(driver.DRV_STATUS(), BIN);
 	String high = digitalRead(EN_PIN) ? " HIGH" : " LOW";
-	String chopper = String(driver.CHOPCONF(), BIN);
-	String lost_steps = String(driver.LOST_STEPS(), BIN);
-	String cool = String(max_power, DEC);
+	String chopper = String(driver.getCurrent());
+	String lost_steps = String(stepper.getRPM());
+	String cool = String(position);
 	File data = SPIFFS.open(logging_file, "r");
 	HTTP.send(200, "application/json",
 			  "STATUS REG: " + status +
 				  "\nSTALL: " + stall +
 				  "\nEN PIN: " + high +
-				  "\nCHOPPER CONF: " + chopper +
-				  "\nCOOL CONF: " + cool +
-				  "\nLOST_STEPS: " + lost_steps + "\n" +
+				  "\nMOTOR CURRENT: " + chopper +
+				  "\nENCODER POS: " + cool +
+				  "\nMOTOR SPEED: " + lost_steps + "\n" +
 				  data.readString());
 	data.close();
 }
 
 void motorUp()
 {
-	target = blinds_top;
+	target = encoder_top;
 	publish_position = true;
 }
 
 void motorDown()
 {
-	target = blinds_bot;
+	target = encoder_bottom;
 	publish_position = true;
 }
 
 void motorStop()
 {
-	target = taken;
+	target = position;
 	publish_position = false;
 }
 
@@ -134,75 +140,84 @@ void mqtt_callback(char *topic, byte *payload, unsigned int length)
 		}
 		else if (response == "RESET")
 		{
-			taken = blinds_top;
-			target = blinds_top;
-			position = blinds_top;
+			target = encoder_top;
 			publish_position = true;
 		}
 	}
 	else if (String(topic) == topic_set_position)
 	{
-		target = map(response.toInt(), 0, 100, 0, blinds_top);
+		target = map(response.toInt(), 0, 100, encoder_bottom, encoder_top);
 	}
 	else if (String(topic) == topic_feedback)
 	{
-		position = map(response.toInt(), 0, 100, 0, blinds_top);
-		taken = map(response.toInt(), 0, 100, 0, blinds_top);
-		target = taken;
+		position = map(response.toInt(), 0, 100, 0, encoder_top);
+		target = position;
 		publish_position = true;
 	}
 }
 
 void stepperLoop()
 {
-	int delta = target - taken;
+	int delta = target - position;
+	if (delta < DEADZONE && delta > -DEADZONE)
+	{
+		stepper.stop();
+		delta = 0;
+	}
 	delta = max(delta, MIN_DELTA);
 	delta = min(delta, MAX_DELTA);
+	output = encoder.getPosition();
+	if ((lastOutput - output) > 2047)
+		revolutions++;
+	if ((lastOutput - output) < -2047)
+		revolutions--;
+	position = revolutions * 4096 + output;
+	lastOutput = output;
 	if (!delta)
 	{
 		digitalWrite(EN_PIN, HIGH);
 		if (publish_position)
 		{
-			position = target == blinds_top ? 100 : 0;
-			client.publish(topic_position, String(position).c_str());
+			long realPos = target == encoder_bottom ? encoder_bottom : position;
+			client.publish(topic_position, String(map(realPos, encoder_top, encoder_bottom, 100, 0)).c_str());
 			publish_position = false;
 		}
 	}
 	else
 	{
 		int power, speed;
-		if (taken < max_power && target == blinds_top)
+		if (position < max_power && target > max_power)
 		{
 			power = 1500;
-			speed = 6000;
+			speed = 30;
 		}
-		else if (taken > max_power && target == blinds_top)
+		else if (position > max_power && target > max_power)
 		{
 			power = 1200;
-			speed = 6000;
+			speed = 30;
 		}
 		else
 		{
 			power = 800;
-			speed = 12000;
+			speed = 90;
 		}
-		driver.SilentStepStick2130(power);
 		stepper.begin(speed, MICROSTEPS);
-		digitalWrite(EN_PIN, LOW);
+		driver.SilentStepStick2130(power);
+		driver.hold_delay(5);
+		driver.hold_current(0);
 		driver.DRV_STATUS();
-		if (driver.otpw())
+		if (driver.drv_err())
 		{
-			stepper.rotate(-delta * 1.5);
-			taken -= 0.5 * delta;
-			driver.SilentStepStick2130(100);
-			delay(5000);
-			return;
+			digitalWrite(EN_PIN, HIGH);
+			lastOvercurrent = millis();
 		}
-		stepper.rotate(delta);
-		taken += delta;
-		position = map(taken, blinds_top, 0, 100, 0);
-		client.publish(topic_position, String(position).c_str());
-		publish_position = true;
+		if (millis() - lastOvercurrent > 5000)
+		{
+			digitalWrite(EN_PIN, LOW);
+			stepper.rotate(delta);
+			client.publish(topic_position, String(map(position, encoder_top, encoder_bottom, 100, 0)).c_str());
+			publish_position = true;
+		}
 	}
 }
 
